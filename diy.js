dddd755ce40ec091b020facbce688142fd08fb1c
Jin Jay <jinjaysnow@gmail.com>
2018-03-21 20:44:07 +0800

    fix download

--- diy.js
+++ diy.js
@@ -1,18 +1,19 @@
 (function(){
-	var oldData='';
+	var oldData=''; // 之前一次的数据
 	var km_fname = '';
 	var html = '';
 	var win_title = '';
-	html += '<a class="diy export" data-type="json">导出json</a>',
-	html += '<a class="diy export" data-type="md">导出md</a>',
-	html += '<a class="diy export" data-type="km">导出km</a>',
+	html += '<a class="diy export" data-type="json" id="json_export">导出json</a>',
+	html += '<a class="diy export" data-type="png" id="png_export">导出png</a>',
+	html += '<a class="diy export" data-type="md" id="md_export">导出md</a>',
+	html += '<a class="diy export" data-type="km" id="km_export">导出km</a>',
 	html += '<button class="diy input">',
 	html += '导入<input type="file" id="fileInput">',
 	html += '</button>',
-	html += '<textarea class="diy input" id="prompt_txt">',
-	html +='</textarea>';    
+	// html += '<textarea class="diy input" id="prompt_txt">',
+	// html +='</textarea>';    
 
-	$('.editor-title').append(html);
+	$('.editor-title').append(html); // 添加按钮
 
 	$('.diy').css({
 		// 'height': '30px',
@@ -40,92 +41,82 @@
 		display: 'inline-block',
 		opacity: 0
 	});
-	$('.export').css('cursor','not-allowed');
+	$('.export').css('cursor','pointer');
 
+	// 导出
 	$(document).on('mouseover', '.export', function(event) {
 		// 链接在hover的时候生成对应数据到链接中
 		event.preventDefault();
-		var $this = $(this),
-				type = $this.data('type'),
-				exportType;
-		switch(type){
-			case 'km':
-				exportType = 'json';
-				break;
-			case 'md':
-				exportType = 'markdown';
-				break;
-			default:
-				exportType = type;
-				break;
-		}
 		if(JSON.stringify(oldData) == JSON.stringify(editor.minder.exportJson())){
+			// 旧数据与现有数据相同
 			return;
 		}else{
 			oldData = editor.minder.exportJson();
 		}
-
-		editor.minder.exportData(exportType).then(function(content){
-			switch(exportType){
-				case 'json':
-					console.log($.parseJSON(content));
-					break;
-				default:
-					console.log(content);
-					break;
-			}
-			$this.css('cursor', 'pointer');
+		editor.minder.exportData('json').then(function(content){
 			var blob = new Blob([content]),
-					url = URL.createObjectURL(blob);
-			var aLink = $this[0];
-			aLink.href = url;
-			aLink.download = $('#node_text1').text()+'.'+type;
-            //$.post("http://127.0.0.1:8080/", {name:'123'}, function(data,status){alert("Data: " + data + "\nStatus: " + status);});
-            //console.log('after post');
+					data_url = URL.createObjectURL(blob);
+			var aLink = $("#json_export")[0];
+			aLink.href = data_url;
+			aLink.download = $('#node_text1').text()+'.json';
 		});
-	}).on('mouseout', '.export', function(event) {
-		// 鼠标移开是设置禁止点击状态，下次鼠标移入时需重新计算需要生成的文件
-		event.preventDefault();
-		//$(this).css('cursor', 'not-allowed');
-	}).on('click', '.export', function(event) {
-		// 禁止点击状态下取消跳转
-		var $this = $(this);
-		if($this.css('cursor') == 'not-allowed'){
-			//event.preventDefault();
-		}
+		editor.minder.exportData('png').then(function(content){
+			var blob = new Blob([content]),
+					data_url = URL.createObjectURL(blob);
+			var aLink = $("#png_export")[0];
+			aLink.href = data_url;
+			aLink.download = $('#node_text1').text()+'.png';
+		});
+		editor.minder.exportData('markdown').then(function(content){
+			var blob = new Blob([content]),
+					data_url = URL.createObjectURL(blob);
+			var aLink = $("#md_export")[0];
+			aLink.href = data_url;
+			aLink.download = $('#node_text1').text()+'.md';
+		});
+		// km实质上是json格式
+		editor.minder.exportData('json').then(function(content){
+			var blob = new Blob([content]),
+					data_url = URL.createObjectURL(blob);
+			var aLink = $("#km_export")[0];
+			aLink.href = data_url;
+			aLink.download = $('#node_text1').text()+'.km';
+		});
+
 	});
 
-	window.setInterval(
-		function(){
-			if (win_title != $('#node_text1').text()) {
-				win_title = $('#node_text1').text();
-				document.title = win_title;
-			}
-			if (oldData == '') {
-				oldData = editor.minder.exportJson();
-				return;
-			}            
-			if(JSON.stringify(oldData) == JSON.stringify(editor.minder.exportJson())){
-				return;
-			}else{
-				oldData = editor.minder.exportJson();
-			}
-			exportType='json';
-			editor.minder.exportData(exportType).then(function(content){                               
-				$.ajax({
-					type: "POST",  
-					url: "upload.php?fname="+encodeURI(km_fname)+"&text="+$('#node_text1').text(), 
-					data: JSON.stringify(content),
-					success: function(msg){
-					//alert( "Data Saved: " + msg ); 
-			myDate = new Date();
-			txt=document.getElementById('prompt_txt');
-			txt.value = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds() + " : " + msg;
-					}
-				});               
-			});
-	        }, 
-        5000);
+  // 不上传
+	// window.setInterval(
+	// 	function(){
+	// 		if (win_title != $('#node_text1').text()) {
+	// 			win_title = $('#node_text1').text();
+	// 			document.title = win_title;
+	// 		}
+	// 		if (oldData == '') {
+	// 			oldData = editor.minder.exportJson();
+	// 			return;
+	// 		}            
+	// 		if(JSON.stringify(oldData) == JSON.stringify(editor.minder.exportJson())){
+	// 			return;
+	// 		}else{
+	// 			oldData = editor.minder.exportJson();
+	// 		}
+	// 		exportType='json';
+	// 		editor.minder.exportData(exportType).then(function(content){                               
+	// 			$.ajax({
+	// 				type: "POST",  
+	// 				url: "upload.php?fname="+encodeURI(km_fname)+"&text="+$('#node_text1').text(), 
+	// 				data: JSON.stringify(content),
+	// 				success: function(msg){
+	// 				//alert( "Data Saved: " + msg ); 
+	// 		myDate = new Date();
+	// 		txt=document.getElementById('prompt_txt');
+	// 		txt.value = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds() + " : " + msg;
+	// 				}
+	// 			});               
+	// 		});
+	//         }, 
+  //       5000);
 
 	// 导入
 	window.onload = function() {
